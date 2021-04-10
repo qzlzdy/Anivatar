32e8cd332bba1122c7b5a9742add77b130e8849b
骑着轮子的羊 <yangtl@hdu.edu.cn>
2021-04-10 17:20:40 +0800

    ADD FROM ARCHLINUX

---
+++ anivatar/core/generate.py
@@ -0,0 +1,97 @@
+import argparse
+import json
+import random
+
+import torch
+from torchvision import utils
+from gen_model import Generator
+
+
+with open('tags.json', 'r') as f:
+    tags = json.load(f)
+tag_ind = {tag:ind for ind,tag in enumerate(tags)}
+def one_hot(sample, lb, device):
+    labels = [0] * len(tags)
+    for tag in lb:
+        labels[tag_ind[tag]] = 1
+    labels = torch.Tensor(labels).float()
+    labels = labels.unsqueeze(0)
+    labels = labels.repeat(sample, 1)
+    return labels.to(device)
+
+with open('sample_labels.json', 'r') as f:
+    sample_labels = json.load(f)
+total_samples = len(sample_labels)
+def get_random_labels(batch, device):
+    labels = []
+    for i in range(batch):
+        labels.append(sample_labels[random.randint(0, total_samples - 1)])
+    return torch.Tensor(labels).float().to(device)
+
+def generate(args, g_ema, device, mean_latent):
+
+    with torch.no_grad():
+        g_ema.eval()
+        for i in range(args.pics):
+            sample_z = torch.randn(args.sample, args.latent, device=device)
+            if len(args.labels) == 0:
+                sample_l = get_random_labels(args.sample, device=device)
+            else:
+                sample_l = one_hot(args.sample, args.labels, device=device)
+
+            sample, _ = g_ema(
+                [sample_z], sample_l, truncation=args.truncation, truncation_latent=mean_latent
+            )
+
+            rows = int(args.sample ** 0.5)
+            rows = args.sample // rows
+            utils.save_image(
+                sample,
+                f"portrait/{str(i).zfill(6)}.png",
+                nrow=rows,
+                normalize=True,
+                range=(-1, 1),
+            )
+
+
+if __name__ == '__main__':
+    device = 'cpu'
+
+    parser = argparse.ArgumentParser(description='Generate samples from the generator')
+
+    parser.add_argument('--size', type=int, default=128)
+    parser.add_argument('--sample', type=int, default=1)
+    parser.add_argument('--pics', type=int, default=16)
+    parser.add_argument('--truncation', type=float, default=1)
+    parser.add_argument('--truncation_mean', type=int, default=4096)
+    parser.add_argument('--ckpt', type=str, default='./checkpoint/600000.pt')
+    parser.add_argument('--channel_multiplier', type=int, default=2)
+    parser.add_argument('--labels', type=str, default=None)
+
+    args = parser.parse_args()
+
+    args.latent = 256
+    args.n_mlp = 8
+
+    if args.labels is None:
+        args.labels = []
+    else:
+        args.labels = args.labels.split(",")
+
+    g_ema = Generator(
+        args.size, 23, args.latent, args.n_mlp, channel_multiplier=args.channel_multiplier
+    ).to(device)
+
+    if args.ckpt is not None:
+        checkpoint = torch.load(args.ckpt)
+
+        g_ema.load_state_dict(checkpoint["g_ema"])
+
+    if args.truncation < 1:
+        labels = one_hot(args.truncation_mean, args.labels, device=device)
+        with torch.no_grad():
+            mean_latent = g_ema.mean_latent(args.truncation_mean, labels)
+    else:
+        mean_latent = None
+
+    generate(args, g_ema, device, mean_latent)
---
+++ anivatar/core/magnify.py
@@ -0,0 +1,36 @@
+import argparse
+from PIL import Image
+
+from torchvision.utils import save_image
+from torchvision.transforms.functional import to_tensor
+
+from mag_models import CARN_V2, network_to_half
+from Common import *
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Waifu2x')
+    
+    parser.add_argument('-o', '--out', type=str, default='out.png')
+    parser.add_argument('input', type=str)
+    
+    args = parser.parse_args()
+    
+    # load model
+    model_cran_v2 = CARN_V2(color_channels=3, mid_channels=64, conv=nn.Conv2d,
+                        single_conv_size=3, single_conv_group=1,
+                        scale=2, activation=nn.LeakyReLU(0.1),
+                        SEBlock=True, repeat_blocks=3, atrous=(1, 1, 1))
+    model_cran_v2 = network_to_half(model_cran_v2)
+    checkpoint = "checkpoint/CARN_model_checkpoint.pt"
+    model_cran_v2.load_state_dict(torch.load(checkpoint, 'cpu'))
+    model_cran_v2 = model_cran_v2.float() 
+
+    img = args.input
+    img = Image.open(img).convert("RGB")
+    img = to_tensor(img).unsqueeze(0) 
+
+    with torch.no_grad():
+
+        out = model_cran_v2(img)
+
+    save_image(out, args.out)
